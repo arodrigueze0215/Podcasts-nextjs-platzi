1f8e9e27fc32895f24cb65a3d79089039bc04ccb
Andres Rodriguez Escudero <andres.rodriguez0215@gmail.com>
2019-01-25 19:06:50 -0500

    firs commit

---
+++ pages/index.js
@@ -0,0 +1,78 @@
+import 'isomorphic-fetch';
+export default class extends React.Component {
+
+    static async getInitialProps() {
+        let req = await fetch('https://api.audioboom.com/channels/recommended');
+        let {body: channels} = await req.json();
+        return { channels };
+    }
+    render(){
+        const { channels } = this.props;
+
+        return <div>
+            <header>podcasts</header>
+            
+
+            <div className="channels">
+                { channels.map((channel) => (
+                <a className="channel" key={ channel.id }>
+                    <img src={ channel.urls.logo_image.original } alt=""/>
+                    <h2>{ channel.title }</h2>
+                </a>
+                )) }
+            </div>
+
+            <style jsx>{`
+                header {
+                    color: #fff;
+                    background: #8756ca;
+                    padding: 15px;
+                    text-align: center;
+                }
+                .channels {
+                    display: grid;
+                    grid-gap: 15px;
+                    padding: 15px;
+                    grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+                }
+                .channel {
+                    display: block;
+                    border-radius: 3px;
+                    box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+                    margin-botton:0.5em;
+                }
+
+                a.channel {
+                    display: block;
+                    margin-bottom: 0.5em;
+                    color: #333;
+                    text-decoration: none;
+                  }
+
+                .channel img {
+                    border-radius: 3px;
+                    box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+                    width:100%;
+                }
+
+                h2 {
+                    padding: 5px;
+                    font-size: 0.9em;
+                    font-weight: 600;
+                    margin: 0;
+                    text-align: center;
+                  }
+            `}
+            </style>
+            <style jsx global>{`
+                body {
+                    margin: 0;
+                    font-family: system-ui;
+                    background: white;
+                }
+
+            `}
+            </style>
+        </div>
+    }
+}
