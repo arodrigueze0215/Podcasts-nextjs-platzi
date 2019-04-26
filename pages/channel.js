e775d28d2eaa82f90629b09813bf88fec713d583
arodrigueze <andres.rodriguez0215@gmail.com>
2019-04-26 18:51:36 -0500

    add status code

--- pages/channel.js
+++ pages/channel.js
@@ -3,30 +3,43 @@ import Series from '../components/Series.jsx';
 import PodcastList from '../components/PodcastList.jsx';
 import Layout from '../components/Layout.jsx';
 import Banner from '../components/Banner.jsx';
+import Error from 'next/error';
 export default class Channel extends React.Component {
 
-    static async getInitialProps({ query }) {
+    static async getInitialProps({ res, query }) {
 
-        let [reqChannel, reqAudios, reqSeries] = await Promise.all([
-            fetch(`https://api.audioboom.com/channels/${query.id}`),
-            fetch(`https://api.audioboom.com/channels/${query.id}/audio_clips`),
-            fetch(`https://api.audioboom.com/channels/${query.id}/child_channels`)
-        ])
+        try {
+            let [reqChannel, reqAudios, reqSeries] = await Promise.all([
+                fetch(`https://api.audioboom.com/channels/${query.id}`),
+                fetch(`https://api.audioboom.com/channels/${query.id}/audio_clips`),
+                fetch(`https://api.audioboom.com/channels/${query.id}/child_channels`)
+            ])
 
-        let dataChannel = await reqChannel.json();
-        let { channel } = dataChannel.body;
-        
-        let dataAudio = await reqAudios.json();
-        let { audio_clips } = dataAudio.body;
-        
-        let dataSerie = await reqSeries.json();
-        let series = dataSerie.body.channels;
-
-        return { channel, audio_clips, series }
+            if (reqChannel.status >= 400) {
+                res.statusCode = reqChannel.status;
+                return {reqChannel: null, reqAudios: null, reqSeries: null, statusCode: res.statusCode}
+            } 
+            
+            let dataChannel = await reqChannel.json();
+            let { channel } = dataChannel.body;
+            
+            let dataAudio = await reqAudios.json();
+            let { audio_clips } = dataAudio.body;
+            
+            let dataSerie = await reqSeries.json();
+            let series = dataSerie.body.channels;
+            
+            return { channel, audio_clips, series, statusCode:reqChannel.status }            
+        } catch (error) {
+            res.statusCode = 503;
+            return { reqChannel: null, reqAudios: null, reqSeries: null, statusCode: 503 }
+        }
     } 
     render() {
-        const { channel, audio_clips, series } = this.props;
-
+        const { channel, audio_clips, series, statusCode } = this.props;
+        if (statusCode !== 200) {
+            return(<Error statusCode={ statusCode }/>)
+        }
         return(
             <Layout title="Podcasts">
                 <Banner channel={ channel }/>
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,26 @@
 import 'isomorphic-fetch';
 import Layout from '../components/Layout.jsx'
 import ChannelGrid from '../components/ChannelGrid.jsx';
+import Error from 'next/error';
 
 export default class extends React.Component {
 
-    static async getInitialProps() {
-        let req = await fetch('https://api.audioboom.com/channels/recommended');
-        let {body: channels} = await req.json();
-        return { channels };
+    static async getInitialProps({ res }) {
+        try {
+            let req = await fetch('https://api.audioboom.com/channels/recommended');
+            let {body: channels} = await req.json();
+            return { channels, statusCode:200 };            
+        } catch (error) {
+            res.statusCode = 503;
+            return { channels:null, statusCode:503 };            
+            
+        }
     }
     render(){
-        const { channels } = this.props;
+        const { channels, statusCode } = this.props;
+        if (statusCode !== 200) {
+            return(<Error statusCode={ statusCode }/>)
+        }
         return (
             <Layout title="Podcasts">
                 <ChannelGrid channels={ channels }/>
